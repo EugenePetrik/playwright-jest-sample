ca4cd4f49fcdffd45ff130be7f70d02781712004
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-05-20 12:59:20 +0300

    Fix logger export

--- config/logger_config.js
+++ config/logger_config.js
@@ -1,11 +1,7 @@
 import winston from 'winston';
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
   level: 'debug',
   format: winston.format.combine(winston.format.simple()),
   transports: [new winston.transports.File({ filename: 'combined.log' })],
 });
-
-module.exports = {
-  logger,
-};
