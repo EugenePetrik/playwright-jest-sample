f555fd7c9dda02ee97238e619db7da8d1e8d3ea6
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-05-20 12:31:32 +0300

    Add BrowserConfig class

--- config/browser_config.js
+++ config/browser_config.js
@@ -3,64 +3,85 @@ import { CONFIG } from './env';
 import { logger } from './logger_config.js';
 import chalk from 'chalk';
 
-const browserName = CONFIG.BROWSER_NAME;
-const deviceName = CONFIG.DEVICE_NAME;
-const width = CONFIG.VIEWPORT_WIDTH;
-const height = CONFIG.VIEWPORT_HEIGHT;
-const isNetworkSubscriptionEnabled = CONFIG.NETWORK_SUBSCRIPTION;
-const headless = CONFIG.HEADLESS;
-const devtools = CONFIG.DEVTOOLS;
-const slowMo = CONFIG.SLOW_MO;
-const defaultViewport = CONFIG.VIEWPORT;
+class BrowserConfig {
+  constructor() {
+    this.browserName = CONFIG.BROWSER_NAME;
+    this.deviceName = CONFIG.DEVICE_NAME;
+    this.width = CONFIG.VIEWPORT_WIDTH;
+    this.height = CONFIG.VIEWPORT_HEIGHT;
+    this.isNetworkSubscriptionEnabled = CONFIG.NETWORK_SUBSCRIPTION;
+    this.headless = CONFIG.HEADLESS;
+    this.devtools = CONFIG.DEVTOOLS;
+    this.slowMo = CONFIG.SLOW_MO;
+    this.defaultViewport = CONFIG.VIEWPORT;
 
-let browser, context, page;
-
-export async function goto(path) {
-  const pageUrl = CONFIG.THE_INTERNET_URL + path;
-  logger.info(`Open page - ${pageUrl}`);
-  await page.goto(pageUrl, { waitUntil: 'load' });
-  return page;
-}
-
-export async function run(viewport = defaultViewport) {
-  browser = await { chromium, webkit, firefox }[browserName].launch({
-    headless,
-    devtools,
-    slowMo,
-  });
-
-  logger.info(`Current browser - ${browserName}`);
-  logger.info(`Current viewport - ${viewport}`);
-
-  switch (viewport) {
-    case 'desktop':
-      context = await browser.newContext({
-        viewport: {
-          width,
-          height,
-        },
-      });
-      page = await context.newPage();
-      break;
-    case 'mobile':
-      logger.info(`Device - ${deviceName}`);
-      context = await browser.newContext({
-        ...devices[deviceName],
-      });
-      page = await context.newPage();
-      break;
-    default:
-      throw new Error('[ERROR] Please, select the browser viewport');
+    this.browser = null;
+    this.context = null;
+    this.page = null;
   }
 
-  if (isNetworkSubscriptionEnabled) {
-    page.on('request', request => logger.debug(chalk.green('>>', request.method(), request.url())));
-    page.on('response', response => logger.debug(chalk.green('<<', response.status(), response.url())));
+  async openBrowser() {
+    this.browser = await { chromium, webkit, firefox }[this.browserName].launch({
+      headless: this.headless,
+      devtools: this.devtools,
+      slowMo: this.slowMo,
+    });
+
+    logger.info(`Browser name - ${this.browserName}`);
+
+    return this.browser;
+  }
+
+  async openBrowserContext(viewport = this.defaultViewport) {
+    logger.info(`Current viewport - ${viewport}`);
+
+    switch (viewport) {
+      case 'desktop':
+        logger.info(`Browser width - ${this.width}, browser height - ${this.height}`);
+        this.context = await this.browser.newContext({
+          viewport: {
+            width: this.width,
+            height: this.height,
+          },
+        });
+        break;
+      case 'mobile':
+        logger.info(`Device - ${this.deviceName}`);
+        this.context = await this.browser.newContext({
+          ...devices[this.deviceName],
+        });
+        break;
+      default:
+        throw new Error('[ERROR] Please, select the browser viewport');
+    }
+
+    if (this.isNetworkSubscriptionEnabled) {
+      page.on('request', request => logger.debug(chalk.green('>>', request.method(), request.url())));
+      page.on('response', response => logger.debug(chalk.green('<<', response.status(), response.url())));
+    }
+
+    return this.context;
+  }
+
+  async openPage() {
+    this.page = await this.context.newPage();
+    return this.page;
+  }
+
+  async closePage() {
+    this.page = await this.page.close();
+    return this.page;
+  }
+
+  async closeBrowserContext() {
+    this.context = await this.context.close();
+    return this.context;
+  }
+
+  async closeBrowser() {
+    this.browser = await this.browser.close();
+    return this.browser;
   }
 }
 
-export async function stop() {
-  await page.close();
-  await context.close();
-  await browser.close();
-}
+export default new BrowserConfig();
